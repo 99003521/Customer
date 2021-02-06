19787d113cf380a21a1d0308f0bbdaae010c79d9
stepin105599 <ragulkrish974@gmail.com>
2021-02-06 14:30:46 +0530

    doc

--- box/test_box.cpp
+++ box/test_box.cpp
@@ -1,12 +1,20 @@
 #include "box.h"
 #include <gtest/gtest.h>
 namespace {
+  /**
+   * @brief Construct a test case for empty constructor
+   * 
+   */
 TEST(Box, Empty_Cosnstructor) {
     Box B1;
   EXPECT_EQ(0, B1.length());
   EXPECT_EQ(0, B1.breadth());
   EXPECT_EQ(0, B1.height());
 }
+/**
+ * @brief Construct a test case for parameterised constructor from given input
+ * 
+ */
 
 TEST(Box, Parameterized_Cosnstructor) {
     Box B1(1,2,3);
@@ -14,7 +22,10 @@ TEST(Box, Parameterized_Cosnstructor) {
   EXPECT_EQ(2, B1.height());
   EXPECT_EQ(3, B1.breadth());
 }
-
+/**
+ * @brief Construct a new TEST case for copy constructor
+ * 
+ */
 TEST(Box, Copy_Cosnstructor) {
     Box B1(1,2,3);
     Box B2(B1);
