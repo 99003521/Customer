42a8c52831eda6fcead9a6085a61ed47d1199bea
Adharsa Mohan R <adharsa1709@gmail.com>
2021-02-06 13:50:28 +0530

    update

--- customer.h
+++ customer.h
@@ -1,4 +1,16 @@
-#ifndef __CUSTOMER_H
+/**
+ * @file customer.h
+ * @author Team1->Aman Kalaskar 
+ *                Aiswarya P S
+ *                Adharsha Mohan R
+ *                Ragul Periaswamy
+ * @brief 
+ * @version 0.1
+ * @date 2021-02-06
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 #define __CUSTOMER_H
 
 #include<string>
@@ -17,13 +29,48 @@ class Customer {
     double balance;
     AccountType type;
     public:
+    /**
+     * @brief Default Constructor for customer
+     * 
+     */
     Customer();
+    /**
+     * @brief Paramaterized constructor for Customer which includes double
+     * 
+     */
     Customer(std::string,std::string,std::string,double,AccountType);
+    /**
+     * @brief Paramaterized constructor for Customer
+     * 
+     */
     Customer(std::string,std::string,std::string,AccountType);
+    /**
+     * @brief Copy constructor for Customer
+     * 
+     */
     Customer(const Customer&);
-    void credit(double);            //recharge or billPay
+    /**
+     * @brief Credit Function
+     * 
+     */
+    void credit(double);     
+    /**
+     * @brief Make call function
+     * 
+     * @return * void 
+     */
     void makeCall(double);
+    /**
+     * @brief Get the Balance object
+     * 
+     * @return double 
+     */
+
     double getBalance() const;
+    /**
+     * @brief Display function
+     * 
+     */
     void dispay() const;
 };
 #endif
