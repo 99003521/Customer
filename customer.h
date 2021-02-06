72c94f1791d0b567de7da648f4497aff86f27c0c
amanajaykalaskar <amanajay.kalaskar@ltts.com>
2021-02-06 10:05:17 +0530

    update3.0

--- customer.h
+++ customer.h
@@ -6,24 +6,24 @@
 #include<iostream>
 
 enum AccountType {
-Prepaid,
-Postpaid
+    Prepaid,
+    Postpaid
 };
 
 class Customer {
-std::string id;
-std::string name;
-std::string phone;
-double balance;
-AccountType type;
-public:
-Customer();
-Customer(std::string,std::string,std::string,double,AccountType);
-Customer(std::string,std::string,std::string,AccountType);
-Customer(const Customer&);
-void credit(double);            //recharge or billPay
-void makeCall(double);
-double getBalance() const;
-void dispay() const;
+    std::string id;
+    std::string name;
+    std::string phone;
+    double balance;
+    AccountType type;
+    public:
+    Customer();
+    Customer(std::string,std::string,std::string,double,AccountType);
+    Customer(std::string,std::string,std::string,AccountType);
+    Customer(const Customer&);
+    void credit(double);            //recharge or billPay
+    void makeCall(double);
+    double getBalance() const;
+    void dispay() const;
 };
 #endif
