f422a8278890c2b44bac4e8a2c406afef87a2d52
stepin105599 <ragulkrish974@gmail.com>
2021-02-06 14:25:46 +0530

    document

--- customer_test.cpp
+++ customer_test.cpp
@@ -1,15 +1,27 @@
 #include "customer.h"
 #include<gtest/gtest.h>
 namespace{
+    /**
+     * @brief Construct a new TEST object for checking the balance
+     * 
+     */
     TEST(Customer,getBalance){
         Customer c1("1","Aman","7972770192",420.0,Prepaid);
         EXPECT_EQ(420.0, c1.getBalance());
     }
+    /**
+     * @brief Construct a new TEST object for adding credit with the balance 
+     * 
+     */
     TEST(Customer,credit){
         Customer c1("1","Aman","7972770192",420.0,Prepaid);
         c1.credit(300.0);
         EXPECT_EQ(720.0, c1.getBalance());
     }
+    /**
+     * @brief Construct a new TEST object for getting the balance after a call has been made
+     * 
+     */
     TEST(Customer,makecall){
         Customer c1("1","Aman","7972770192",420.0,Prepaid);
         c1.makeCall(4);
